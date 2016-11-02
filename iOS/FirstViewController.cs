7b8466ba8603435f09c2e4bb1c396a7d43825594
Jean D'Amore <jdamore@thoughtworks.com>
2016-11-02 23:22:11 +1000

    Moved all code away from root ViewController

---
+++ iOS/FirstViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Foundation;
+
+using UIKit;
+
+namespace phoneword.iOS
+{
+	public partial class FirstViewController : UIViewController
+	{
+		FirstView view;
+
+		private void translateEventHandler(object sender, EventArgs e)
+		{
+			string translatedNumber = "";
+			translatedNumber = PhoneTranslator.ToNumber(view.GetPhonewordText().Text);
+			view.GetPhonewordText().ResignFirstResponder();
+			if (translatedNumber == "")
+			{
+				view.GetCallButton().SetTitle("Call ", UIControlState.Normal);
+				view.GetCallButton().Enabled = false;
+			}
+			else {
+				view.GetCallButton().SetTitle("Call " + translatedNumber, UIControlState.Normal);
+				view.GetCallButton().Enabled = true;
+			}
+		}
+
+		private void callEventHandler(object sender, EventArgs e)
+		{
+			string translatedNumber = "";
+			translatedNumber = PhoneTranslator.ToNumber(view.GetPhonewordText().Text);
+			var url = new NSUrl("tel:" + translatedNumber);
+
+			if (!UIApplication.SharedApplication.OpenUrl(url))
+			{
+				var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+				alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+				PresentViewController(alert, true, null);
+			}
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			view = FirstView.Create();
+			view.Frame = View.Frame;
+			view.GetTranslateButton().TouchUpInside += this.translateEventHandler;
+			view.GetCallButton().TouchUpInside += this.callEventHandler;
+			View.AddSubview(view);
+
+		}
+	}
+}
--- iOS/ViewController.cs
+++ iOS/ViewController.cs
@@ -7,46 +7,13 @@ namespace phoneword.iOS
 {
 	public partial class ViewController : UIViewController
 	{
-		FirstView firstView;
-
-		private void translateEventHandler(object sender, EventArgs e)
-		{
-			string translatedNumber = "";
-			translatedNumber = PhoneTranslator.ToNumber(firstView.GetPhonewordText().Text);
-			firstView.GetPhonewordText().ResignFirstResponder();
-			if (translatedNumber == "")
-			{
-				firstView.GetCallButton().SetTitle("Call ", UIControlState.Normal);
-				firstView.GetCallButton().Enabled = false;
-			}
-			else {
-				firstView.GetCallButton().SetTitle("Call " + translatedNumber, UIControlState.Normal);
-				firstView.GetCallButton().Enabled = true;
-			}
-		}
-
-		private void callEventHandler(object sender, EventArgs e)
-		{
-			string translatedNumber = "";
-			translatedNumber = PhoneTranslator.ToNumber(firstView.GetPhonewordText().Text);
-			var url = new NSUrl("tel:" + translatedNumber);
-
-			if (!UIApplication.SharedApplication.OpenUrl(url))
-			{
-				var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
-				alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
-				PresentViewController(alert, true, null);
-			}
-		}
-
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			firstView = FirstView.Create();
-			firstView.Frame = View.Frame;
-			firstView.GetTranslateButton().TouchUpInside += this.translateEventHandler;
-			firstView.GetCallButton().TouchUpInside += this.callEventHandler;
-			View.AddSubview(firstView);
+
+			FirstViewController firstViewController = new FirstViewController();
+			//PresentViewController(firstViewController, true, null);
+			View.AddSubview(firstViewController.View);
 		}
 
 		public override void DidReceiveMemoryWarning()
